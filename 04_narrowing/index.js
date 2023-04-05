a979b75af3f12ae79914043d3cacc55be77f2fcf
weslleypmfortunato <111170704+weslleypmfortunato@users.noreply.github.com>
2023-04-05 14:07:43 -0700

    exercise narrowing

--- 04_narrowing/index.js
+++ 04_narrowing/index.js
@@ -14,7 +14,7 @@ function sum(a, b) {
 sum("4", "59");
 sum(12, 42.3);
 sum("5", 6);
-// 2 - Checando se o valoe existe
+// 2 - Checando se o valor existe
 function operations(arr, operation) {
     if (operation) {
         if (operation === "sum") {
@@ -79,3 +79,36 @@ function showDogDetails(dog) {
 }
 showDogDetails(turca);
 showDogDetails(bob);
+function ShowReview(score) {
+    if (!score) {
+        console.log("A sua avaliação é muito importante para nós. caso decida nos avaliar no futuro, entre na sua área logada e clique em 'AVALIAR CURSO'");
+        return;
+    }
+    else if (typeof score === "number") {
+        switch (score) {
+            case 1:
+                console.log(`Nota: ${score}. Obrigado por seu feedback. Vamos reaviliar nosso método de ensino`);
+                break;
+            case 2:
+                console.log(`Nota: ${score}. Obrigado pelo seu feedback. Nossos tutores serão informados dos pontos de melhoria sugeridos`);
+                break;
+            case 3:
+                console.log(`Sentimos muito que o curso não tenha lhe agradado. Vamos rever seus comentários. Fique a vontade para entrar em contato`);
+                break;
+            case 4:
+                console.log(`Nota: ${score}. Seu feedback foi muito importante para nossa equipe. Esperamos ver você em breve em um dos nossos outros cursos.`);
+                break;
+            case 5:
+                console.log(`Nota: ${score}. Estamos muito felizes que atendemos suas expectativas. Aguardamos você nos próximos cursos.`);
+                break;
+            default:
+                console.log(`Nota: ${score}. Desculpe, as notas só podem ser de 1 a 5, sendo 1 para muito insatisfeito e 5 para muito satisfeito`);
+        }
+    }
+}
+ShowReview(1);
+ShowReview(2);
+ShowReview(3);
+ShowReview(4);
+ShowReview(5);
+ShowReview(false);
